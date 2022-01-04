a539f0823155e9e95eb92938d5ea9b1b0bd87794
UnluckyBoy <qfmy_zhang@163.com>
2022-01-04 22:57:37 +0800

    001-push

--- Script/index.js
+++ Script/index.js
@@ -1,13 +1,18 @@
+/**
+ * DOM加载完直接执行，用于加载图片等
+ */
 $(function() {
     //decodeURIComponent解编码，用 location.search 来获取传递值
     var str = decodeURIComponent(location.search.slice(1).split("=")[1])
     console.log("Json:" + str)
-    var Json_Obj = $.parseJSON(str); //Json转Odj
+    if (str != null || str != "") {
+        var Json_Obj = $.parseJSON(str); //Json转Odj
+        //console.log("Json转对象:" + Json_Obj)
+        $("#uName").text(Json_Obj.name + ",欢迎您！"); //绑定用户名
 
-    $("#uName").text(Json_Obj.name + ",欢迎您！"); //绑定用户名
-
-    //绑定头像url
-    var handUrl = "http://localhost:8080/getImage" + Json_Obj.image;
-    console.log("hand_url:" + handUrl)
-    $("#handImage").attr("src", handUrl);
+        //绑定头像url
+        var handUrl = "http://localhost:8080/getImage" + Json_Obj.image;
+        console.log("hand_url:" + handUrl)
+        $("#handImage").attr("src", handUrl);
+    }
 });
--- Script/loginScript.js
+++ Script/loginScript.js
@@ -1,13 +1,26 @@
 document.querySelector('.img-btn').addEventListener('click', function() {
     document.querySelector('.cont').classList.toggle('s-signup')
 });
-window.onload = function() {
-    var mInputs = document.getElementsByTagName("input");
-    var login_btn = document.getElementById("login-btn");
-    var register_btn = document.getElementById("register-btn");
 
-    login_btn.onclick = function() {
-        if ((!mInputs[0].value || !mInputs[1].value)) {
+$(function() {
+    var login = document.getElementById("login-btn");
+    var register = document.getElementById("register-btn");
+
+    //获取input框内的值
+
+    /*
+    var mAccount = $("#account").val();
+    var mPassword = $("#password").val();
+    var name = $("#up_name").val();
+    var account = $("#up_account").val();
+    var password = $("#up_password").val();
+    var confirm = $("#up_confirm").val(); 
+    */
+
+    login.onclick = function() {
+        if (($("#account").val() == null || $("#account").val() == "") ||
+            ($("#password").val() == null || $("#password").val() == "")) {
+            console.log($("#account").val());
             alert("输入内容不能为空");
         } else {
             $.ajax({
@@ -19,6 +32,8 @@ window.onload = function() {
                 success: function(result) {
                     if (result != null) {
                         console.log(result);
+                        //登录成功，保存cookie
+                        //document.cookie = result.token;
                         window.location.href = "../index.html?data=" + window.encodeURIComponent(JSON.stringify(result)); //将result转Json传输
                         //alert("To-Json: " + JSON.stringify(result));
                     }
@@ -30,9 +45,41 @@ window.onload = function() {
         }
     }
 
-    register_btn.onclick = function() {
-        if ((!mInputs[0].value || !mInputs[1].value)) {
+    /**
+     * 注册按钮握手数据
+     */
+    register.onclick = function() {
+        if (($("#up_name").val() == null || $("#up_name").val() == "") ||
+            ($("#up_account").val() == null || $("#up_account").val() == "") ||
+            ($("#up_password").val() == null || $("#up_password").val() == "") ||
+            ($("#up_confirm").val() == null || $("#up_confirm").val() == "")) {
             alert("输入内容不能为空");
+        } else if ($("#up_password").val() == $("#up_confirm").val()) {
+            $.ajax({
+                method: "post",
+                url: "http://localhost:8080/UserInfo/register",
+                dataType: "json",
+                async: false,
+                data: "name=" + $("#up_name").val() + "&account=" + $("#up_account").val() + "&password=" + $("#up_password").val(),
+                success: function(resultData) {
+                    if (resultData != null) {
+                        var mJson = $.parseJSON(JSON.stringify(resultData));
+                        console.log(mJson.result + "_的类型:" + typeof(mJson.result));
+                        if (mJson.result == "false") {
+                            //已注册
+                            alert("此账号已注册，请检查！");
+                        } else {
+                            //尚未注册，获取注册结果，跳转主页
+                            window.location.href = "../index.html?data=" + window.encodeURIComponent(JSON.stringify(resultData));
+                        }
+                    }
+                },
+                error: function(msg) {
+
+                }
+            });
+        } else {
+            alert("两次密码输入异常，请检查！");
         }
     }
-}
+});
